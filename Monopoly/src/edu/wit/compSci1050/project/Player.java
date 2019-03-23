e2047bda1ea714fa4807423ce1516c5765d048cf
drewLabrie <44929671+drewLabrie@users.noreply.github.com>
2019-03-23 18:17:19 -0400

    Began work on Player class and added a constructor

--- Monopoly/src/edu/wit/compSci1050/project/Player.java
+++ Monopoly/src/edu/wit/compSci1050/project/Player.java
@@ -1,5 +1,15 @@
 package edu.wit.compSci1050.project;
 
 public class Player {
+	
+	private String name;
+	
+	private int money;
+	
+	Player(String name,int money){
+		this.name = name;
+		this.money = money;
+		
+	}
 
 }
