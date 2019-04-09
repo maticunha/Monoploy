9fa2593c9cc1b2f1577714fc15dd79f36a666259
joshlevine55 <46460705+joshlevine55@users.noreply.github.com>
2019-04-09 18:09:31 -0400

    Added scrollpane, textfield and anchorpane

--- Monopoly/src/edu/wit/compSci1050/project/Run.java
+++ Monopoly/src/edu/wit/compSci1050/project/Run.java
@@ -7,6 +7,9 @@ import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -14,6 +17,12 @@ public class Run extends Application implements Initializable {
 	public static String player1_name;
 	public static String player2_name;
 	public static String player3_name;
+	
+	@FXML
+	ScrollPane GameLog; 
+	
+	@FXML
+	TextField Log; 
 	
 	@FXML
 	static ImageView Player1; 
