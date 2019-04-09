08a3f4bba7dd14593ea87a3e600a41d2165f2dbf
joshlevine55 <46460705+joshlevine55@users.noreply.github.com>
2019-04-09 16:09:41 -0400

    Missing pieces that share names

--- Monopoly/src/edu/wit/compSci1050/project/Run.java
+++ Monopoly/src/edu/wit/compSci1050/project/Run.java
@@ -15,12 +15,115 @@ public class Run extends Application implements Initializable {
 	public static String player1;
 	public static String player2;
 	public static String player3;
-	public static VBox[] vboxes = { 
+	public static ImageView[] images = { 
 			
 	};
 	
 	@FXML 
-	ImageView Go; 
+	ImageView Go;
+	
+	@FXML 
+	ImageView Assembly; 
+	
+	@FXML
+	
+	ImageView CommunityChest;
+	
+	@FXML
+	ImageView Perl;
+	
+	@FXML
+	ImageView JavaScript; 
+	
+	@FXML
+	ImageView Question;
+	
+	@FXML
+	ImageView HTML5; 
+	
+	@FXML
+	ImageView Tax;
+	
+	@FXML
+	ImageView Java; 
+	
+	@FXML
+	ImageView Scratch;
+	
+	@FXML
+	ImageView Jail;
+	
+	@FXML
+	ImageView Ruby; 
+	
+	@FXML
+	ImageView Chrome;
+	
+	@FXML
+	ImageView Scala;
+	
+	@FXML
+	ImageView JSON;
+	
+	@FXML
+	ImageView Python; 
+	
+	@FXML
+	ImageView VB; 
+	
+	@FXML
+	ImageView FireFox;
+	
+	@FXML
+	ImageView OpenGL;
+	
+	@FXML
+	ImageView FreeParking; 
+	
+	@FXML
+	ImageView PHP;
+	
+	@FXML
+	ImageView Lua; 
+	
+	@FXML
+	ImageView InternetExplorer; 
+	
+	@FXML
+	ImageView R; 
+	
+	@FXML
+	ImageView CPlus;
+	
+	@FXML
+	ImageView Windows; 
+	
+	@FXML
+	ImageView PowerShell; 
+	
+	@FXML
+	ImageView GoToJail;
+	
+	@FXML
+	ImageView MATLAB; 
+	
+	@FXML
+	ImageView Razer; 
+	
+	@FXML
+	ImageView Delphi; 
+	
+	@FXML
+	ImageView Eclipse; 
+	
+	@FXML
+	ImageView Swift; 
+	
+	@FXML
+	ImageView CSharp; 
+	
+	@FXML
+	ImageView OBJC; 
 	
 	public static void main (String[] args) {
 		VBox[] vboxes = { 
