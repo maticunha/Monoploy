0ff33fab361c35ee9e1e41dfdbb8b1f4bb66bba6
maticunha <cunham1@wit.edu>
2019-04-09 15:21:13 -0400

    Created everything needed in Run

--- Monopoly/src/edu/wit/compSci1050/project/Run.java
+++ Monopoly/src/edu/wit/compSci1050/project/Run.java
@@ -1,17 +1,38 @@
 package edu.wit.compSci1050.project;
 
 
-import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Run {
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Run extends Application implements Initializable {
 	public static String player1;
 	public static String player2;
 	public static String player3;
+	public static VBox[] vboxes = { 
+			
+	};
 	
 	public static void main (String[] args) {
 		VBox[] vboxes = { 
 				
 		};
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
